b9e54c84c10c0bc81814a0b6d6aa3f08a66a9a89
maanasmahato4 <maanasmahato456@gmail.com>
2023-12-17 23:46:27 +0545

    new: authentication routes

---
+++ src/routes/index.js
@@ -0,0 +1,20 @@
+// 3rd party libraries
+const express = require("express");
+
+// custom imports
+const {
+    RegisterUser,
+    SignInUser,
+    SignOutUser,
+    RefreshAccessToken
+} = require("../controllers/authentication.controller");
+
+// routes
+const router = express.Router();
+
+// authentication routes
+router
+    .post("/auth/register", RegisterUser)
+    .post("/auth/signin", SignInUser)
+    .post("/auth/signout", SignOutUser)
+    .post("/auth/refresh", RefreshAccessToken)
