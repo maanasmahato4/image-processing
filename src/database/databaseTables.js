5d1673eec1305a05edcc96b3b4ec3bb4a652e004
maanasmahato4 <maanasmahato456@gmail.com>
2023-12-19 13:44:32 +0545

    new: added few tables

--- src/database/databaseTables.js
+++ src/database/databaseTables.js
@@ -3,10 +3,25 @@
 // 3rd party libraries
 const pool = require("../database/database");
 
-async function createUsersTable(){
-    return await pool.query('CREATE TABLE IF NOT EXISTS USERS (USERNAME VARCHAR(255), EMAIL VARCHAR(255), PASSWORD TEXT)');
+async function createUsersTable() {
+    return await pool.query('CREATE TABLE IF NOT EXISTS USERS (ID SERIAL PRIMARY KEY, USERNAME VARCHAR(255), EMAIL VARCHAR(255), PASSWORD TEXT)');
+};
+
+async function createRefreshTokenTable() {
+    return await pool.query('CREATE TABLE IF NOT EXISTS REFRESH_TOKENS (ID SERIAL PRIMARY KEY, UID INT, REFRESH_TOKEN TEXT)');
+};
+
+async function createImagesTables(){
+    return;    
+};
+
+async function createProcessedImagesTable(){
+    return;
 };
 
 module.exports = {
-    createUsersTable
+    createUsersTable,
+    createRefreshTokenTable,
+    createImagesTables,
+    createProcessedImagesTable
 };
