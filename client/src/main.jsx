eecf2121865d14df4957092ee46177b7664e096b
maanasmahato4 <maanasmahato456@gmail.com>
2023-12-20 15:33:30 +0545

    new: sign up form

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom/client';
 import Layout from './Layout';
 import { MantineProvider } from "@mantine/core";
 import { BrowserRouter } from "react-router-dom";
+import { AuthContextProvider } from "./context/auth.context";
 import '@mantine/core/styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <MantineProvider>
-      <BrowserRouter>
-        <Layout />
-      </BrowserRouter>
+      <AuthContextProvider>
+        <BrowserRouter>
+          <Layout />
+        </BrowserRouter>
+      </AuthContextProvider>
     </MantineProvider>
   </React.StrictMode>,
 )
