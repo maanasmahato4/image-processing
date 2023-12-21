a66e2d1e9aeeb2528329ef4dd2f5f235ce7e4905
maanasmahato4 <maanasmahato456@gmail.com>
2023-12-21 22:09:35 +0545

    update: signin done

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,11 +1,15 @@
-import { useEffect } from "react";
+import { useContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { Button, Container, Flex, TextInput } from "@mantine/core";
 import { useForm } from "@mantine/form";
 import { EMAIL_REGEXP } from "../constants/RegExp";
+import { validatePassword } from "../helpers/validatePassword";
+import { privateAPI } from "../api/axios";
+import { AppContext } from "../context/context";
 
 function SignIn() {
     const navigate = useNavigate();
+    const { setAccessToken, isAuthenticated, setIsAuthenticated } = useContext(AppContext);
 
     const signInForm = useForm({
         initialValues: {
@@ -20,14 +24,34 @@ function SignIn() {
         }
     });
 
+    async function handleSignInFormSubmit(values) {
+        try {
+            const user = validatePassword(values);
+            const response = await privateAPI.post("/auth/signin", user);
+            if (!response || response.status !== 200) {
+                throw new Error(`Error ${response ? response.status : "No response"}`);
+            };
+            const { status, access_token } = response.data;
+            if (status === "success") {
+                setAccessToken(access_token);
+                setIsAuthenticated(true);
+            };
+        } catch (error) {
+            console.error(`An Error occured: ${error}`);
+        };
+    };
+    
+
     useEffect(() => {
-        console.log("protected route is left in login");
-    }, []);
+        if (isAuthenticated) {
+            navigate("/");
+        };
+    }, [isAuthenticated]);
 
     return (
         <Container>
             <h1 style={{ textAlign: "center" }}>Login to your account</h1>
-            <form onSubmit={signInForm.onSubmit((values) => console.log(values))} style={{ width: "60%", marginInline: "20%", marginBlock: "2rem" }} onReset={signInForm.onReset}>
+            <form onSubmit={signInForm.onSubmit((values) => handleSignInFormSubmit(values))} style={{ width: "60%", marginInline: "20%", marginBlock: "2rem" }} onReset={signInForm.onReset}>
                 <TextInput label="Email" type="email" name="email" placeholder="abc@example.com" {...signInForm.getInputProps("email", { type: "input" })} withAsterisk />
                 <TextInput label="Password" type="password" name="password" placeholder="password must be at least 8 characters long" {...signInForm.getInputProps("password", { type: "input" })} withAsterisk />
                 <TextInput label="Confirm Password" type="password" name="confirm_password" placeholder="password must be at least 8 characters long" {...signInForm.getInputProps("confirm_password", { type: "input" })} withAsterisk />
